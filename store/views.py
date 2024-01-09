972d76d250618313004608e5821ca6478e520446
Ayat-sidielkhir <121581791+Ayat-sidielkhir@users.noreply.github.com>
2024-01-09 17:23:14 +0100

    ayat

--- store/views.py
+++ store/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def store(request, category_slug=None):
-    categories = Category.objects.all()
-    products = Product.objects.filter(is_available=True)
+    categories = None
+    products = None
+    paged_products = None  # Define it here so that it's accessible in the context
 
-    category = None  # Initialize category outside the if condition
-
-    if category_slug:
+    if category_slug is not None:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 6)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
+        product_count = products.count()
 
-    product_count = products.count()
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count': product_count,
         'categories': categories,
-        'current_category': category,
+        'current_category': category if 'category' in locals() else None,
     }
     return render(request, 'store/store.html', context)
+
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
@@ -31,4 +39,5 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
 
