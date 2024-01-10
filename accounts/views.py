f5e6e829b5a2580e3fd8d79603d0f58415fcfa5c
Imane CHEMLAL <104566157+CHEMLAL-Imane@users.noreply.github.com>
2024-01-10 10:17:25 +0100

    imane

--- accounts/views.py
+++ accounts/views.py
@@ -9,13 +9,13 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            phone_number = form.cleaned_data['phone_number']
+            # last_name = form.cleaned_data['last_name']
+            # phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             username = email.split("@")[0]
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
-            user.phone_number = phone_number
+            # user.phone_number = phone_number
             user.save()
             messages.success(request,'Registration successful')
             return redirect('register')
